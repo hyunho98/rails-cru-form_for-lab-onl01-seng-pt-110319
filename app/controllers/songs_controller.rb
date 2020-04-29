f9a6ec3bd8fb36aa139d9fde3be2d3f88cf2f4b7
Alex Lee <hyunho98@gmail.com>
2020-04-29 09:43:15 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,4 +27,5 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     @song.update(song_params(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
 end
